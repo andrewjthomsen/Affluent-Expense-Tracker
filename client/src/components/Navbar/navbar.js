58f9f16576dded1f61e570bd05ad3cec3d820a64
andrew <andrewjthomsen@yahoo.com>
2019-06-02 10:16:03 -0700

    Fixed routing between pages

--- client/src/components/Navbar/navbar.js
+++ client/src/components/Navbar/navbar.js
@@ -1,14 +1,14 @@
 import React from "react";
+import { Link } from 'react-router-dom';
 import "./navbar.css";
 class Navbar extends React.Component {
 	render() {
 		return (
 			<div>
 				<ul className="nav">
-					<li className="nav-item slam-left"><a href="#">Home</a></li>
-					<li className="nav-item"><a href="#">Home</a></li>
-					<li className="nav-item"><a href="#">About</a></li>
-					<li className="nav-item"><a className="contact" href="#">Contact</a></li>
+					<li className="nav-item slam-left"><Link to="/">Home</Link></li>
+					<li className="nav-item"><Link to="/expenses">Add Expense</Link></li>
+					<li className="nav-item"><Link to="/balance">Balance</Link></li>
 				</ul>
 			</div>
 			
